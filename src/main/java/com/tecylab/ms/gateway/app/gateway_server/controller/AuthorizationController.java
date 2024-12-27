92c481a01065007ec46504ba6ec205e1acc86a0f
Fernando <asialer05@hotmail.com>
2024-12-26 20:50:12 -0500

    eliminando codigo

--- src/main/java/com/tecylab/ms/gateway/app/gateway_server/controller/AuthorizationController.java
+++ src/main/java/com/tecylab/ms/gateway/app/gateway_server/controller/AuthorizationController.java
@@ -1,17 +1,6 @@
 package com.tecylab.ms.gateway.app.gateway_server.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Map;
-
-@RestController
 public class AuthorizationController {
-    @GetMapping("/authorized")
-    public Map<String,String> authorized(@RequestParam String code){
-        return  Collections.singletonMap("code",code);
-    }
+
 
 }
